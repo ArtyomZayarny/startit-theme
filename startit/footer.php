d7699fd8197b3f8ddd5690c14999e575b43186ca
ArtyomZayarny <artyom.zayarny@gmail.com>
2018-12-21 00:01:47 +0200

    init theme
    theme for wordpress

---
+++ startit/footer.php
@@ -0,0 +1,22 @@
+<footer id="footer" class="footer">
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="copyright">
+                <p> &copy; 2018 - carrby.agency. Created by <a href="index.html#">WP ThemeBooster</a> All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- All JavaScript Files
+================================================== -->
+<?php $path = get_template_directory_uri();  ?>
+<script src="<?php echo $path; ?>/js/jquery-3.2.1.min.js"></script>
+<script src="<?php echo $path; ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo $path; ?>/js/jquery.mixitup.min.js"></script>
+<script src="<?php echo $path; ?>/js/jquery.fancybox.min.js"></script>
+<script src="<?php echo $path; ?>/js/owl.carousel.js"></script>
+<script src="<?php echo $path; ?>/js/typed.min.js"></script>
+<script src="<?php echo $path; ?>/js/menu.js"></script>
+<script src="<?php echo $path; ?>/js/custom.js"></script>
---
+++ startit/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <meta name="description" content="Carrby - Agency Template">
+    <meta name="author" content="">
+
+    <!-- Page Title -->
+    <title>Carrby - Agency Template</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="<?php echo get_template_directory_uri();?>/images/favicon.png" rel="shortcut icon" type="image/png">
+
+    <!-- Lead Style -->
+    <link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet" type="text/css">
+
+</head>
